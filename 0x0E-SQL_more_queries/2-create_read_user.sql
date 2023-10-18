e2c1230ccdff4c0f75de9b8835da02f7cc4c4ce8
Amanuel <amanuel.demirew@yahoo.com>
2023-10-18 11:45:25 +0300

    trial new 1

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
-GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO user_0d_2@localhost;
