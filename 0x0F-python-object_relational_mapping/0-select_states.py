0a2654fd798a897cb500b87a2a4e1e707ca74fa3
Amanuel <amanuel.demirew@yahoo.com>
2023-11-18 22:46:10 +0300

    trial 4

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Lists all states."""
+""" lists all states from the known database hbtn_0e_usa """
+
 import MySQLdb
 import sys
 
 
-def main():
+
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
